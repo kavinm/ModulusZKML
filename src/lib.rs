7b323e82cd4f420f13efff157f0460d582b6ade4
Nick Cosby <nick@dissonantchat.com>
2023-06-06 16:17:57 -0700

    ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![warn(missing_docs)]
 //!Remainder: A fast GKR based library for building zkSNARKS for ML applications
 
-pub mod mle;
 pub mod expression;
 pub mod layer;
+pub mod mle;
 pub mod sumcheck;
