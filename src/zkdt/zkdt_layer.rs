5f97830884c0973531f5424253b27f9410bfd18b
Nick Cosby <nick@dissonantchat.com>
2023-07-31 13:23:50 -0700

    Minor change to zkdt_layer.rs

--- src/zkdt/zkdt_layer.rs
+++ src/zkdt/zkdt_layer.rs
@@ -21,6 +21,7 @@ impl<F: FieldExt> LayerBuilder<F> for ProductTreeBuilder<F> {
         // Create flatmle from tuple mle
         let mut flat_mle: DenseMle<F, F> = self.mle.into_iter().map( |(first, second)| first * second).collect();
         flat_mle.add_prefix_bits(&prefix_bits.unwrap());
+        flat_mle.define_layer_id(id);
         flat_mle
     }
 
