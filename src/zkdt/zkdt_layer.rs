6fe774f7c43175ed7b8820473d1f5f2d5e2ae5ba
agxmbhir <agambhir5656@gmail.com>
2023-08-02 06:00:34 +0530

    fix layer builder

--- src/zkdt/zkdt_layer.rs
+++ src/zkdt/zkdt_layer.rs
@@ -1,20 +1,18 @@
 use ark_bn254::Fr;
 
+use crate::expression::{ExpressionStandard, Expression};
+use crate::layer::{Layer, LayerBuilder, LayerId};
+use crate::mle::dense::{DenseMle, DenseMleRef, Tuple2};
+use crate::mle::{zero::ZeroMleRef, Mle, MleIndex, MleRef};
 use crate::FieldExt;
-use crate::layer::{LayerBuilder, LayerId, Layer};
-use crate::expression::{ExpressionStandard }; 
-use crate::mle::{MleIndex, zero::ZeroMleRef, Mle, MleRef};
-use crate::mle::dense::{DenseMle, Tuple2, DenseMleRef};
 
 use super::structs::BinDecomp16Bit;
 
-
-struct ProductTreeBuilder<F:FieldExt> {
+struct ProductTreeBuilder<F: FieldExt> {
     mle: DenseMle<F, Tuple2<F>>,
-} 
+}
 
-impl<F: FieldExt> LayerBuilder<F> for ProductTreeBuilder<F> { 
-
+impl<F: FieldExt> LayerBuilder<F> for ProductTreeBuilder<F> {
     type Successor = DenseMle<F, F>;
     //a function that multiplies the parts of the tuple pair-wise
     fn build_expression(&self) -> ExpressionStandard<F> {
@@ -23,41 +21,47 @@ impl<F: FieldExt> LayerBuilder<F> for ProductTreeBuilder<F> {
 
     fn next_layer(&self, id: LayerId, prefix_bits: Option<Vec<MleIndex<F>>>) -> Self::Successor {
         // Create flatmle from tuple mle
-        let mut flat_mle: DenseMle<F, F> = self.mle.into_iter().map( |(first, second)| first * second).collect();
+        let mut flat_mle: DenseMle<F, F> = self
+            .mle
+            .into_iter()
+            .map(|(first, second)| first * second)
+            .collect();
         flat_mle.add_prefix_bits(prefix_bits);
         flat_mle.define_layer_id(id);
         flat_mle
     }
 }
 
-
-struct BinaryDecompBuilder<F:FieldExt> {
+struct BinaryDecompBuilder<F: FieldExt> {
     mle: DenseMle<F, BinDecomp16Bit<F>>,
 }
 
 impl<F: FieldExt> LayerBuilder<F> for BinaryDecompBuilder<F> {
-    
-        type Successor = ZeroMleRef<F>;
-        
-        // Returns an expression that checks if the bits are binary. 
-        fn build_expression(&self) -> ExpressionStandard<F> {
-            let decomp_bit_mle = self.mle.mle_bit_refs();
-            let b = ExpressionStandard::Mle(decomp_bit_mle[0].clone());
+    type Successor = ZeroMleRef<F>;
 
-            let b_squared = ExpressionStandard::Product(vec![
-                decomp_bit_mle[0].clone(),
-                decomp_bit_mle[0].clone(),
-            ]);
-             
-            // b * (1 - b) = b - b^2 
-            b - b_squared
-        }
-    
-        fn next_layer(&self, id: LayerId, prefix_bits: Option<Vec<MleIndex<F>>>) -> Self::Successor {
-            ZeroMleRef::new(self.mle.num_vars(), prefix_bits, id)
-        }
+    // Returns an expression that checks if the bits are binary.
+    fn build_expression(&self) -> ExpressionStandard<F> {
+        let decomp_bit_mle = self.mle.mle_bit_refs();
+
+        // Split the list of expressions into 8 chunks
+        let chunks = decomp_bit_mle.chunks(2);
+
+        // Concatenate the two elements of each chunk and reduce it 4 times
+
+        let final_expression = chunks.map(
+            |chunk| {
+                let first_b = ExpressionStandard::Mle(chunk[0].clone());
+                let second_b = ExpressionStandard::Mle(chunk[1].clone());
+                let first_squared = ExpressionStandard::Product(vec![chunk[0].clone(), chunk[0].clone()]);
+                let second_squared = ExpressionStandard::Product(vec![chunk[1].clone(), chunk[1].clone()]);
+                (first_b - first_squared).concat(second_b - second_squared)
+            }
+        ).reduce(|a, b| a.concat(b)).unwrap();
+
+        final_expression
+    }
+
+    fn next_layer(&self, id: LayerId, prefix_bits: Option<Vec<MleIndex<F>>>) -> Self::Successor {
+        ZeroMleRef::new(self.mle.num_vars() + 4, prefix_bits, id)
+    }
 }
-
-
-
-
