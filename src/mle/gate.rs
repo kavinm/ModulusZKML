cf3a79d9c25e348220d48960aeaa36e6d5e99e26
Vishruti Ganesh <vishruti@mit.edu>
2023-08-07 13:29:49 -0700

    gate

---
+++ src/mle/gate.rs
@@ -0,0 +1,121 @@
+use std::{
+    cmp::max,
+    fmt::Debug,
+    iter::{Cloned, Zip},
+    marker::PhantomData,
+};
+
+use ark_ff::BigInt;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ark_std::{cfg_into_iter, cfg_iter, rand::Rng};
+use itertools::Itertools;
+use rayon::{
+    prelude::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator},
+    slice::ParallelSlice,
+};
+
+use crate::{layer::Claim, mle::beta::BetaTable, expression::{ExpressionStandard, Expression}};
+use crate::FieldExt;
+
+use super::{Mle, MleIndex, MleRef, MleAble, dense::{DenseMleRef, DenseMle}};
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone)]
+
+
+pub struct AddGate<F: FieldExt> {
+    layer_claim: Claim<F>,
+    nonzero_gates: Vec<((F, F, F), F)>,
+    lhs: DenseMleRef<F>,
+    rhs: DenseMleRef<F>,
+    beta_g: Option<BetaTable<F>>,
+}
+
+/// Error handling for beta table construction
+#[derive(Error, Debug, Clone)]
+pub enum GateError {
+    #[error("after phase 1, the lhs should be fully fixed")]
+    Phase1Error,
+}
+
+impl<F: FieldExt> AddGate<F> {
+    /// Initialize Bookkeeping tables for lhs 
+    pub fn init_phase_1(&mut self) -> (DenseMleRef<F>, DenseMleRef<F>) {
+        let mut beta_g = self.beta_g;
+        if beta_g.is_none() { 
+            beta_g = Some(BetaTable::new(self.layer_claim).unwrap());
+            self.beta_g = beta_g;
+        } else {
+            beta_g = self.beta_g;
+        }
+        let num_x = self.lhs.num_vars();
+        let mut a_hg_lhs = vec![F::zero(); 1 << num_x];
+        let mut a_hg_rhs = vec![F::zero(); 1 << num_x];
+        let _ = self.nonzero_gates.clone().into_iter().map(
+            |((z, x, y), val)| {
+                let x_ind = x.into_bigint().as_ref()[0] as usize;
+                let y_ind = y.into_bigint().as_ref()[0] as usize;
+                let z_ind = z.into_bigint().as_ref()[0] as usize;
+                let adder = val * *beta_g.unwrap().table.bookkeeping_table().get(z_ind).unwrap_or(&F::zero());
+                a_hg_lhs[x_ind] = a_hg_lhs[x_ind] + adder;
+                a_hg_rhs[x_ind] = a_hg_rhs[x_ind] + adder * self.rhs.bookkeeping_table().get(y_ind).unwrap_or(&F::zero());
+            }
+        );
+        (DenseMle::new(a_hg_lhs).mle_ref(), DenseMle::new(a_hg_rhs).mle_ref())
+    }
+
+    pub fn init_phase_2(&self, phase_1_claim_chal: Vec<F>, f_at_u: F) -> (DenseMleRef<F>, DenseMleRef<F>){
+        let beta_g = self.beta_g.as_ref().expect("beta table should be initialized by now");
+        // uhhhhhh
+        let phase_1_claim = (phase_1_claim_chal, F::zero()); 
+        let beta_u = BetaTable::new(phase_1_claim).unwrap();
+        let num_y = self.rhs.num_vars();
+        let mut a_f1_lhs = vec![F::zero(); 1 << num_y];
+        let mut a_f1_rhs = vec![F::zero(); 1 << num_y];
+        let _ = self.nonzero_gates.clone().into_iter().map(
+            |((z, x, y), val)| {
+                let x_ind = x.into_bigint().as_ref()[0] as usize;
+                let y_ind = y.into_bigint().as_ref()[0] as usize;
+                let z_ind = z.into_bigint().as_ref()[0] as usize;
+                let gz = *beta_g.table.bookkeeping_table().get(z_ind).unwrap_or(&F::zero());
+                let ux = *beta_u.table.bookkeeping_table().get(x_ind).unwrap_or(&F::zero());
+                a_f1[x_ind] = a_f1[x_ind] + gz * ux * val * f_at_u;
+            }
+        );
+        (DenseMle::new(a_f1).mle_ref(), DenseMle::new(a_f1).mle_ref())
+    }
+
+    pub fn prep_for_sumcheck(&mut self, phase_1_claim: Claim<F>) -> Result<(DenseMleRef<F>, DenseMleRef<F>), GateError> {
+        // do first u rounds of sumcheck on lhs
+        let chal = self.init_phase_1();
+        if self.lhs.bookkeeping_table().len() != 1 { 
+            return Err(GateError::Phase1Error);
+        } 
+        else {
+            let f_at_u = self.lhs.bookkeeping_table()[0];
+            let (a_f1, a_f3) = self.init_phase_2(chal, f_at_u);
+            let mut expr = ExpressionStandard::Sum(Box::new(ExpressionStandard::Mle(a_f1.clone())), Box::new(ExpressionStandard::Product(vec![a_f1.clone(), a_f3.clone()])));
+            let num_y = self.rhs.num_vars();
+            let new_chal = vec![F::one(); num_y];
+            let final_val = expr.evaluate_expr(new_chal);
+            dbg!(final_val);
+            Ok((a_f1, a_f3))
+        }
+    }
+
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::mle::{dense::DenseMle, Mle};
+
+    use super::*;
+    use ark_bn254::Fr;
+    use ark_std::One;
+
+    #[test]
+    fn test_expected_val() {
+
+    }
+}
