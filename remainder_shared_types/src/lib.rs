9b023f84a95e6cbf0139fb5a6bbd029ff001c226
nick-moduluslabs <nick@moduluslabs.xyz>
2023-10-18 13:36:59 -0700

    fixes

--- remainder_shared_types/src/lib.rs
+++ remainder_shared_types/src/lib.rs
@@ -1,24 +1,26 @@
 pub mod transcript;
 
+pub use poseidon::Poseidon;
+
 use std::hash::Hash;
 
-use halo2curves::ff::{PrimeField, FromUniformBytes, Field, WithSmallOrderMulGroup};
+use halo2curves::FieldExt as H2FieldExt;
 use serde::{Deserialize, Serialize};
 
 pub use halo2curves::bn256::Fr;
-pub use poseidon::Poseidon;
+// pub use poseidon::Poseidon;
 
 pub use halo2curves;
 
 ///External definition of Field element trait, will remain an Alias for now
-pub trait FieldExt: PrimeField + Serialize + for<'de> Deserialize<'de> + FromUniformBytes<64> + WithSmallOrderMulGroup<3> + std::cmp::Ord + Hash {
-    fn zero() -> Self {
-        Self::ZERO
-    }
+pub trait FieldExt: H2FieldExt + Serialize + for<'de> Deserialize<'de> + std::cmp::Ord + Hash {
+    // fn zero() -> Self {
+    //     Self::ZERO
+    // }
 
-    fn one() -> Self {
-        Self::ONE
-    }
+    // fn one() -> Self {
+    //     Self::ONE
+    // }
 
     fn to_bytes_le(&self) -> Vec<u8> {
         self.to_repr().as_ref().to_vec()
@@ -30,14 +32,14 @@ pub trait FieldExt: PrimeField + Serialize + for<'de> Deserialize<'de> + FromUni
         Self::from_repr(repr).unwrap()
     }
 
-    fn get_lower_128(&self) -> u128 {
-        let bytes = self.to_bytes_le();
-        let mut lower_64 = 0u128;
-        for (i, byte) in bytes.into_iter().enumerate().take(8) {
-            lower_64 |= (byte as u128) << (i * 8);
-        }
-        lower_64
-    }
+    // fn get_lower_128(&self) -> u128 {
+    //     let bytes = self.to_bytes_le();
+    //     let mut lower_64 = 0u128;
+    //     for (i, byte) in bytes.into_iter().enumerate().take(8) {
+    //         lower_64 |= (byte as u128) << (i * 8);
+    //     }
+    //     lower_64
+    // }
 }
 
-impl<F: PrimeField + Serialize + for<'de> Deserialize<'de> + FromUniformBytes<64> + WithSmallOrderMulGroup<3>+ std::cmp::Ord + Hash> FieldExt for F {}
+impl<F: H2FieldExt + Serialize + for<'de> Deserialize<'de> + std::cmp::Ord + Hash> FieldExt for F {}
