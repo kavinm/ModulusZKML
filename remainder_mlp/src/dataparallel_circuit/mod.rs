ac33891a5071e66096b977129d1cca43d283bf7b
ende <endeshen99@gmail.com>
2024-03-26 22:46:39 -0700

    cleanup

--- remainder_mlp/src/dataparallel_circuit/mod.rs
+++ remainder_mlp/src/dataparallel_circuit/mod.rs
@@ -2,6 +2,6 @@ pub mod utils;
 pub mod dataparallel_circuit_builders;
 pub mod dataparallel_bits_are_binary_builder;
 pub mod dataparallel_relu_builder;
-pub mod dataparallel_nn_full_circuit;
+// pub mod dataparallel_nn_full_circuit;
 pub mod dataparallel_bias_builder;
 pub mod dataparallel_partial_recomp_builder;
--- remainder_mlp/src/dims.rs
+++ remainder_mlp/src/dims.rs
@@ -1,18 +1,18 @@
 use remainder::mle::{bin_decomp_structs::bin_decomp_64_bit::BinDecomp64Bit, dense::DenseMle};
 use remainder_shared_types::FieldExt;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct NNLinearDimension {
     pub in_features: usize,
     pub out_features: usize,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct NNLinearInputDimension {
     pub num_features: usize,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct NNLinearWeights<F: FieldExt> {
     pub weights_mle: DenseMle<F, F>,    // represent matrix on the right, note this is the A^T matrix from: https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
                                         // ^ its shape is (in_features, out_features)
@@ -20,7 +20,7 @@ pub struct NNLinearWeights<F: FieldExt> {
     pub dim: NNLinearDimension,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct MLPWeights<F: FieldExt> {
     pub all_linear_weights_biases: Vec<NNLinearWeights<F>>,
 }
@@ -28,14 +28,14 @@ pub struct MLPWeights<F: FieldExt> {
 /// Not batched
 type ReluWitness<F> = DenseMle<F, BinDecomp64Bit<F>>;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct MLPInputData<F: FieldExt> {
     pub input_mle: DenseMle<F, F>,
     pub dim: NNLinearInputDimension,
     pub relu_bin_decomp: Vec<ReluWitness<F>>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct DataparallelMLPInputData<F: FieldExt> {
     pub input_mles: Vec<DenseMle<F, F>>,
     pub dim: NNLinearInputDimension,
